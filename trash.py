4702ce61d58dc25c753ba9a8f6596d9fd9c6147a
Vasilii Tolstoi <eternalwanderer13@yahoo.com>
2023-01-04 02:04:57 +0600

    func read and write were added

--- trash.py
+++ trash.py
@@ -9,3 +9,19 @@ class File:
 
     def remove(self):
         print(f'Файл {self.name} был удален')
+
+    def read(self):
+        if self.is_deleted:
+            print(f'ErrorReadFileDeleted({self.name})')
+        elif self.in_trash:
+            print(f'ErrorReadFileTrashed({self.name})')
+        else:
+            print(f'Прочитали все содержимое файла {self.name}')
+
+    def write(self, content):
+        if self.is_deleted:
+            print(f'ErrorWriteFileDeleted({self.name})')
+        elif self.in_trash:
+            print(f'ErrorWriteFileTrashed({self.name})')
+        else:
+            print(f'Записали значение {content} в файл {self.name}')
