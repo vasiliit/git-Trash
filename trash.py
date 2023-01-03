b01ca620de938bdaf88ab52b0accadffb32d9577
Vasilii Tolstoi <eternalwanderer13@yahoo.com>
2023-01-04 02:03:08 +0600

    added func restore_from trash and remove

--- trash.py
+++ trash.py
@@ -3,3 +3,9 @@ class File:
         self.name = name
         self.in_trash = False
         self.is_deleted = False
+
+    def restore_from_trash(self):
+        print(f'Файл {self.name} восстановлен из корзины')
+
+    def remove(self):
+        print(f'Файл {self.name} был удален')
